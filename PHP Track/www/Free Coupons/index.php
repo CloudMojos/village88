a63189223e9417fdec70a7dabe648485331fbba2
starfart69 <joshuel.simbulan@gmail.com>
2024-01-30 13:38:35 +0800

    yes

--- PHP Track/www/Free Coupons/index.php
+++ PHP Track/www/Free Coupons/index.php
@@ -1,51 +1,21 @@
-<?php 
-    session_start();
-
-    
-    $customer_left = 10;
-
-    if(!isset($_SESSION['counter'])) {
-        $_SESSION['counter'] = 1;
-    } 
-
-    if(!isset($_SESSION['customer_left'])) {
-        $_SESSION['customer_left'] = 10;
-    }
-
-    var_dump($_SESSION);
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Free Coupons</title>
-    <style>
-        * {
-            text-align: center;
-        }
-
-        input {
-            margin: 0px auto;
-            display: block;
-        }
-    </style>
+    <title>Feedback Form</title>
 </head>
 <body>
-    <h1>Welcome Customer!</h1>
-    <h3>We're giving away free coupons as token of appreciation.</h3>
-<?php 
-    $nth_customer = $_SESSION['customer_left'] - $_SESSION['counter'];
-    if ($nth_customer > 0) {
-        echo "<p>$nth_customer remaining! </p>";
-    } 
-?>
 
-    <form action="result.php" method="POST">
-        <label for="name">Kindly type your name:</label>
-        <input type="text" name="name" id="name">
-        <input type="submit" value="submit">
-    </form>
+<form action="result.php" method="post">
+    <label for="name">Your Name (optional):</label><br>
+    <input type="text" id="name" name="name"><br><br>
+
+    <label for="phone">Contact number:</label><br>
+    <input type="tel" id="phone" name="phone" pattern="(\+63|0)9\d{9}" placeholder="+639 or 09" required>
+
+    <input type="submit" value="Submit">
+</form>
+
 </body>
 </html>
--- PHP Track/www/Free Coupons/result.php
+++ PHP Track/www/Free Coupons/result.php
@@ -1,43 +1,19 @@
-<?php 
-    session_start();
+<?php
+$name = $_POST['name'];
+$course = $_POST['phone'];
 
-    $coupon = [];
-    for ($i = 0; $i < 7; $i++) {
-        $coupon[$i] = rand(0, 9);
-    }
-    $coupon = implode('', $coupon);
 
-    $_SESSION['counter'] += 1;
 
-    var_dump($_SESSION);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang='en'>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Free Coupons</title>
-    <style>
-        * {
-            text-align: center;
-        }
-
-        div {
-            margin: 0px auto;
-            width: 30vw;
-            border-radius: 15px;
-            border: 1px dashed black;
-        }
-    </style>
+<meta charset='UTF-8'>
+<title>Submitted Entry</title>
 </head>
-<body>
-    <h1>Welcome Customer!</h1>
-    <h3>We're giving away free coupons as token of appreciation.</h3>
 
-    <div>
-        <h6>50% Discount</h6>
-        <h1><?= $coupon ?></h1>
-    </div>
+<body>
+
 </body>
 </html>
