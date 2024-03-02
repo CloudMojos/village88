03741542f941928e4692534ea0271807dc5af02e
starfart69 <joshuel.simbulan@gmail.com>
2024-03-02 18:39:54 +0800

    house.php

--- PHP Track/www/items.php
+++ PHP Track/www/items.php
@@ -27,6 +27,7 @@
             } else {
                 echo "Out of stock<br>";
             }
+            return $this;
         }
 
         public function return() {
@@ -37,26 +38,23 @@
             } else {
                 echo "No items sold<br>";
             }
+            return $this;
         }
     }
 
     $item1 = new Item("Item 1", 10, 5);
-    $item1->buy();
-    $item1->buy();
-    $item1->buy();
-    $item1->return();
+    $item1->buy()->buy()->buy()->return();
     $item1->logDetails();
 
+    echo "<br>";
+
     $item2 = new Item("Item 2", 20, 3);
-    $item2->buy();
-    $item2->buy();
-    $item2->return();
-    $item2->return();
+    $item2->buy()->buy()->return()->return();
     $item2->logDetails();
 
+    echo "<br>";
+
     $item3 = new Item("Item 3", 30, 4);
-    $item3->return();
-    $item3->return();
-    $item3->return();
+    $item3->return()->return()->return();
     $item3->logDetails();
 ?>
